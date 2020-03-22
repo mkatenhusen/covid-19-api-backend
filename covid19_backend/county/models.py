5343be8472fa41894e6d1f00811c67048882bcb1
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 17:06:31 +0100

    fixed ags bug: ags is now a charfield instead of int to keep the leading zeros

--- covid19_backend/county/models.py
+++ covid19_backend/county/models.py
@@ -6,10 +6,13 @@ class State(models.Model):
     name = models.CharField(max_length=30, unique=True)
     slug = models.SlugField(max_length=30, unique=True, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
+
 
 class County(models.Model):
     name = models.CharField(max_length=90, unique=True)
-    ags = models.IntegerField(unique=True, help_text=_("Amtlicher Gemeindeschlüssel"))
+    ags = models.CharField(unique=True, help_text=_("Amtlicher Gemeindeschlüssel"))
 
     gen = models.CharField(max_length=90, blank=True, null=True, help_text=_("Kurzbezeichnung"))
     bez = models.CharField(max_length=200, blank=True, null=True,
@@ -24,3 +27,6 @@ class County(models.Model):
 
     class Meta:
         unique_together = ("name", "state", "ags")
+
+    def __str__(self):
+        return self.name
