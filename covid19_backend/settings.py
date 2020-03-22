fb3de10fe5e2ff2ded5d0eee244783a50f063c2b
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 13:31:13 +0100

    added static

--- covid19_backend/settings.py
+++ covid19_backend/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 #################################
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
