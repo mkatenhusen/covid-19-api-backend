505c603ab11c634b97db11a2c93c29e52fc3167f
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 13:04:53 +0100

    added static

--- covid19_backend/settings.py
+++ covid19_backend/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'covid19_backend.urls'
