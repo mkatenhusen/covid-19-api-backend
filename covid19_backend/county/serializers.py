83ccce46538ac5773a037c14d2f5556bffb0c038
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 11:50:59 +0100

    added posts for county endpoint in order to create new objects

--- covid19_backend/county/serializers.py
+++ covid19_backend/county/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 
 from covid19_backend.county.models import State, County
 
@@ -10,8 +11,15 @@ class StateSerializer(serializers.ModelSerializer):
 
 
 class CountySerializer(serializers.ModelSerializer):
-    state = serializers.CharField(source="state.name", read_only=True)
+    state = serializers.CharField(source="state.name", read_only=False)
 
     class Meta:
         model = County
         fields = ("name", "slug", "ags", "state", "alternative_name", "description", "population", "population_density")
+
+    def create(self, validated_data):
+        try:
+            validated_data["state"] = State.objects.get(name=validated_data.get("state").get("name"))
+            return super().create(validated_data)
+        except State.DoesNotExist:
+            raise serializers.ValidationError({"message": _("The state object does not exist")})
--- covid19_backend/county/views.py
+++ covid19_backend/county/views.py
@@ -1,10 +1,18 @@
 from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from covid19_backend.county.models import County
 from covid19_backend.county.serializers import CountySerializer
 
 
-class CountyView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class CountyView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = CountySerializer
     queryset = County.objects.all()
     lookup_field = "slug"
+    authentication_classes = [TokenAuthentication]
+
+    @permission_classes([IsAuthenticated])
+    def create(self, request, *args, **kwargs):
+        return super().create(request, args, kwargs)
