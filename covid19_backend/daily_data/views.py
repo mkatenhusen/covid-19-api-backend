e942e24dc53a15a3928c96f23ee1a2a8175d3c44
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 15:50:47 +0100

    added "latest" endpoinnt to retrieve the latest daily case or daily gender_age list

--- covid19_backend/daily_data/views.py
+++ covid19_backend/daily_data/views.py
@@ -1,8 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from covid19_backend.daily_data.filters import DayDateFilter
 from covid19_backend.daily_data.models import DailyCase, GenderAgeRelation, Gender, Age
@@ -17,17 +18,25 @@ class DailyCasesView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.Cr
     filterset_fields = ["date_day"]
     authentication_classes = [TokenAuthentication]
 
-
     def get_queryset(self):
         if not self.kwargs:
             queryset = DailyCase.objects.all()
         else:
-            queryset = DailyCase.objects.filter(**self.kwargs)
+            queryset = DailyCase.objects.filter(**self.kwargs).order_by("-date_day")
         return queryset
 
     @permission_classes([IsAuthenticated])
     def create(self, request, *args, **kwargs):
         return super().create(request, args, kwargs)
+
+    @action(detail=False, url_path="latest", methods=["get"])
+    def latest(self, request, *args, **kwargs):
+        latest_case = DailyCase.objects.filter(**self.kwargs).order_by("-date_day")
+        if latest_case:
+            serializer = self.get_serializer(latest_case[0], many=False)
+            return Response(serializer.data)
+        else:
+            return Response({})
 
 
 class GenderAgeView(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -43,12 +52,22 @@ class GenderAgeView(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Gen
         if not self.kwargs:
             queryset = GenderAgeRelation.objects.all()
         else:
-            queryset = GenderAgeRelation.objects.filter(**self.kwargs)
+            queryset = GenderAgeRelation.objects.filter(**self.kwargs).order_by("-date_day")
         return queryset
 
     @permission_classes([IsAuthenticated])
     def create(self, request, *args, **kwargs):
         return super().create(request, args, kwargs)
+
+    @action(detail=False, url_path="latest", methods=["get"])
+    def latest(self, request, *args, **kwargs):
+            latest_dates = GenderAgeRelation.objects.only("date_day").filter(**self.kwargs).order_by("-date_day")
+            if not latest_dates:
+                return Response([])
+            latest_gender_age_relations = GenderAgeRelation.objects.filter(**self.kwargs,
+                                                                           date_day=latest_dates[0].date_day)
+            serializer = self.get_serializer(latest_gender_age_relations, many=True)
+            return Response(serializer.data)
 
 
 # class GenderView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
