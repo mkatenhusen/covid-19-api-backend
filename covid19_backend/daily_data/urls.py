28695c2a5beb4195bfa60ff10475c9cf121708b5
Marcel <marcel.katenhusen@gmx.de>
2020-03-22 12:00:05 +0100

    removed outcommented imports

--- covid19_backend/daily_data/urls.py
+++ covid19_backend/daily_data/urls.py
@@ -1,7 +1,7 @@
 from rest_framework_nested import routers
 
 from covid19_backend.county.urls import county_router
-from covid19_backend.daily_data.views import DailyCasesView, GenderAgeView, AgeView, GenderView
+from covid19_backend.daily_data.views import DailyCasesView, GenderAgeView
 
 county_data_router = routers.NestedDefaultRouter(county_router, r'county', lookup='county_')
 county_data_router.register(r'cases', DailyCasesView, basename='cases')
